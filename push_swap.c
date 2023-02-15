4f30988efa17864740d0a06d88beec085259e781
552020 <stefanolombardo@posteo.de>
2023-02-16 00:16:51 +0100

    chore: the algorithm works with an hard coded array of three elements
    I impplemented sa, sb, ra, pa and pb. It was kind of fun

--- push_swap.c
+++ push_swap.c
@@ -6,32 +6,71 @@ void	ft_ra(int *stack_a, int size);
 int		ft_find_smallest(int *stack_a, int size);
 void	ft_print_stack(int *stack, int size);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void ft_pb(int *stack_a, int *size_a, int *stack_b, int *size_b);
+void ft_pa(int *stack_b, int *size_b, int *stack_a, int *size_a);
+/*
+typedef struct stack {
+    int *data;
+    int size;
+} stack_t;
+*/
+
 
 int main(void)
 {
 	int smallest;
-
 	// Take arguments from the command line.
-    int stack_a[] = {5, 1, 3};
-    int stack_b[3];
-    printf("stack_a before: ");
-	ft_print_stack(stack_a, 3);
+  int stack_a[] = {5, 1, 3};
+  int stack_b[3];
+	int size_a = 3;
+	int size_b = 0;
+  printf("stack_a begin: ");
+	ft_print_stack(stack_a, size_a);
 	// We need now to sort the numbers in the stack A
 	// - We can look for the smallest number, bring it to the top, push it to B
 	// 1. find smallest number in the array
-	smallest = ft_find_smallest(stack_a, 3);
-	printf("smallest: %d\n", smallest);
-	// 2. rotate till the stack_a[0] matches the numbers (ra)
-	ft_ra(stack_a, 3);
-    printf("stack_a after ra: ");
-	ft_print_stack(stack_a, 3);
-	// 3. push the number to the stack_b (pa)
-	// 4. repeat till there are numbers
+	while (1)
+	{
+		smallest = ft_find_smallest(stack_a, size_a);
+		//printf("smallest: %d\n", smallest);
+		// 2. rotate till the stack_a[0] matches the numbers (ra)
+		//printf("size_a main loop: %d\n", size_a);
+		printf("stack_a before ra: ");
+		ft_print_stack(stack_a, size_a);
+		while (stack_a[0] != smallest)
+		{
+			//printf("ft_ra - while\n");
+			ft_ra(stack_a, size_a);
+		}
+		printf("stack_a after ra: ");
+		ft_print_stack(stack_a, size_a);
+		printf("before pa\n");
+		printf("stack a: ");
+		ft_print_stack(stack_a, size_a);
+		printf("stack b: ");
+		ft_print_stack(stack_b, size_b);
+		// 3. push the number to the stack_b (pa)
+		ft_pa(stack_b, &size_b, stack_a, &size_a);
+		printf("after pa\n");
+		printf("stack a: ");
+		ft_print_stack(stack_a, size_a);
+		printf("stack b: ");
+		ft_print_stack(stack_b, size_b);
+		// 4. repeat till there are numbers in stack A;s
+		if (size_a == 0)
+			break ;
+		//else 
+			//printf("loop again\n");
+			//printf("size_a: %d\n", size_a);
+	}
 	// 5. push the numbers back to to stack_b (pb)
-	// We would need just to imagine these operations, not to perform them: our output is not the sorted array but the operations we need to sort it. 
-    ft_sa(stack_a);
-    printf("stack_a after: ");
-	ft_print_stack(stack_a, 3);
+	while (size_b > 0)
+	{
+		//printf("ft_pb while\n");
+		ft_pb(stack_a, &size_a, stack_b, &size_b);
+	}
+  printf("stack_a end: ");
+	ft_print_stack(stack_a, size_a);
     return (0);
 }
 
@@ -52,22 +91,56 @@ void ft_sb(int *stack_b)
     tmp = stack_b[0];
     stack_b[0] = stack_b[1];
     stack_b[1] = tmp;
-    printf("sa\n");
+    printf("sb\n");
 }
 
 void	ft_ra(int *stack_a, int size)
 {
 	int tmp;
-	printf("ft_ra\n");
+	//printf("ft_ra\n");
 	tmp = stack_a[0];
-	printf("tmp: %d\n", tmp);
-	printf("stack_a before ft_memmove: ");
-	ft_print_stack(stack_a, 3);
+	//printf("tmp: %d\n", tmp);
+	//printf("stack_a before ft_memmove: ");
+	//ft_print_stack(stack_a, 3);
 	ft_memmove(&stack_a[0], &stack_a[1], (size -1) * sizeof(int));
-	printf("stack_a after ft_memmove: ");
-	ft_print_stack(stack_a, 3);
+	//printf("stack_a after ft_memmove: ");
+	//ft_print_stack(stack_a, 3);
 	stack_a[size - 1] = tmp;
 	printf("ra\n");
+}
+/* (push a): Take the first element at the top of a and put it at the top of b. Do nothing if a is empty*/
+void ft_pa(int *stack_b, int *size_b, int *stack_a, int *size_a)
+{
+	int tmp;
+	//printf("ft_pa - size_a before: %d\n", *size_a);
+	if (*size_a == 0)
+		return ;
+	tmp = stack_a[0];
+	ft_memmove(&stack_a[0], &stack_a[1], (*size_a - 1) * sizeof(int));
+	printf("ft_pa: stack_a after memmove: ");
+	ft_print_stack(stack_a, *size_a);
+	ft_memmove(&stack_b[1], &stack_b[0], (*size_b) * sizeof(int));
+	stack_b[0] = tmp;
+	(*size_a)--;
+	//printf("ft_pa - size_a after: %d\n", *size_a);
+	(*size_b)++;
+	printf("pa\n");
+}
+
+
+void ft_pb(int *stack_a, int *size_a, int *stack_b, int *size_b)
+{
+	int tmp;
+	
+	if (*size_b == 0)
+		return ;
+	tmp = stack_b[0];
+	ft_memmove(&stack_b[0], &stack_b[1], (*size_b - 1) * sizeof(int));
+	ft_memmove(&stack_a[1], &stack_a[0], (*size_a) * sizeof(int));
+	stack_a[0] = tmp;
+	(*size_b)--;
+	(*size_a)++;
+	printf("pb\n");
 }
 
 int	ft_find_smallest(int *stack_a, int size)
