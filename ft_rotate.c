633013296f67673c08b3600de2ff0fa631a93a3e
552020 <stefanolombardo@posteo.de>
2023-03-20 21:44:59 +0100

    add ft_rb and ft_rrb

--- ft_rotate.c
+++ ft_rotate.c
@@ -6,7 +6,7 @@
 /*   By: slombard <slombard@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 12:03:13 by slombard          #+#    #+#             */
-/*   Updated: 2023/02/24 15:09:03 by slombard         ###   ########.fr       */
+/*   Updated: 2023/03/20 21:44:28 by slombard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,28 @@ void	ft_rra(int *stack_a, int size_a)
 	ft_memmove(&stack_a[1], &stack_a[0], (size_a - 1) * sizeof(int));
 	stack_a[0] = tmp;
 	printf("rra\n");
+}
+
+void	ft_rb(int *stack_b, int size_b)
+{
+	int tmp;
+	//printf("ft_ra\n");
+	tmp = stack_b[0];
+	//printf("tmp: %d\n", tmp);
+	//printf("stack_a before ft_memmove: ");
+	//ft_print_stack(stack_a, 3);
+	ft_memmove(&stack_b[0], &stack_b[1], (size_a -1) * sizeof(int));
+	//printf("stack_a after ft_memmove: ");
+	//ft_print_stack(stack_a, 3);
+	stack_b[size_b - 1] = tmp;
+	printf("rb\n");
+}
+
+void	ft_rrb(int *stack_b, int size_b)
+{
+	int tmp;
+	tmp = stack_b[size_b - 1 ];
+	ft_memmove(&stack_b[1], &stack_b[0], (size_b - 1) * sizeof(int));
+	stack_b[0] = tmp;
+	printf("rrb\n");
 }
