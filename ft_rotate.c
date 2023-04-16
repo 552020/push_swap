3dd21613eaf41759dc57243d792681e2ea5afec2
552020 <stefanolombardo@posteo.de>
2023-04-16 20:14:01 +0200

    add ft_rr and ft_rrr

--- ft_rotate.c
+++ ft_rotate.c
@@ -6,7 +6,7 @@
 /*   By: slombard <slombard@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 12:03:13 by slombard          #+#    #+#             */
-/*   Updated: 2023/03/21 17:39:36 by slombard         ###   ########.fr       */
+/*   Updated: 2023/04/16 20:12:27 by slombard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	ft_ra(int *stack_a, int size_a)
 {
 	int tmp;
+
 	tmp = stack_a[0];
 	ft_memmove(&stack_a[0], &stack_a[1], (size_a -1) * sizeof(int));
 	stack_a[size_a - 1] = tmp;
@@ -24,6 +25,7 @@ void	ft_ra(int *stack_a, int size_a)
 void	ft_rra(int *stack_a, int size_a)
 {
 	int tmp;
+
 	tmp = stack_a[size_a - 1 ];
 	ft_memmove(&stack_a[1], &stack_a[0], (size_a - 1) * sizeof(int));
 	stack_a[0] = tmp;
@@ -33,6 +35,7 @@ void	ft_rra(int *stack_a, int size_a)
 void	ft_rb(int *stack_b, int size_b)
 {
 	int tmp;
+
 	tmp = stack_b[0];
 	ft_memmove(&stack_b[0], &stack_b[1], (size_b -1) * sizeof(int));
 	stack_b[size_b - 1] = tmp;
@@ -42,8 +45,37 @@ void	ft_rb(int *stack_b, int size_b)
 void	ft_rrb(int *stack_b, int size_b)
 {
 	int tmp;
-	tmp = stack_b[size_b - 1 ];
+
+	tmp = stack_b[size_b - 1];
 	ft_memmove(&stack_b[1], &stack_b[0], (size_b - 1) * sizeof(int));
 	stack_b[0] = tmp;
 	ft_printf("rrb\n");
 }
+
+void	ft_rr(int *stack_a, int size_a, int *stack_b, int size_b)
+{
+	int tmp;
+
+	tmp = stack_a[0];
+	ft_memmove(&stack_a[0], &stack_a[1], (size_a -1) * sizeof(int));
+	stack_a[size_a - 1] = tmp;
+	tmp = stack_b[0];
+	ft_memmove(&stack_b[0], &stack_b[1], (size_b -1) * sizeof(int));
+	stack_b[size_b - 1] = tmp;
+	ft_printf("rr\n");
+}
+
+void	ft_rrr(int *stack_a, int size_a, int *stack_b, int size_b)
+{
+	int tmp;
+
+	tmp = stack_a[size_a - 1];
+	ft_memmove(&stack_a[1], &stack_a[0], (size_a - 1) * sizeof(int));
+	stack_a[0] = tmp;
+	tmp = stack_b[size_b - 1];
+	ft_memmove(&stack_b[1], &stack_b[0], (size_b - 1) * sizeof(int));
+	stack_b[0] = tmp;
+	ft_printf("rrr\n");
+}
+
+
